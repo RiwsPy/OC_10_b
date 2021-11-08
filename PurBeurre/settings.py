dcb2b88f6e6f3bd83656b180c509a4794d036bc0
RiwsPy <thibault.debruyne@hotmail.fr>
2021-11-08 09:09:31 +0100

    update I

--- PurBeurre/settings.py
+++ PurBeurre/settings.py
@@ -15,16 +15,19 @@ from dotenv import load_dotenv
 import os
 import dj_database_url
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+import raven
+
 # SECURITY WARNING: don't run with debug turned on in production!
+load_dotenv()
 if os.getenv('ENV') == 'PRODUCTION':
     DEBUG = False
 else:
-    load_dotenv()
     DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -32,7 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+# os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS=['127.0.0.1', '165.232.112.10']
 
 
 # Application definition
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'debug_toolbar',
+    'raven.contrib.django.raven_compat',
 
     'catalogue.apps.CatalogueConfig',
     'user.apps.UserConfig',
@@ -63,7 +68,6 @@ MIDDLEWARE = [
 
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PurBeurre.urls'
@@ -95,7 +99,7 @@ DATABASES = {
         'NAME': os.getenv('SQL_TABLE_NAME'),
         'USER': os.getenv('SQL_USER_NAME'),
         'PASSWORD': os.getenv('SQL_PASSWORD'),
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -153,24 +157,67 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = ['127.0.0.1']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = ''
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 FIXTURE_DIRS = ['catalogue', 'user']
 
 if os.getenv('ENV') == 'PRODUCTION':
-
     # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    """
-    STATICFILES_STORAGE = \
-        'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    """
     STATICFILES_STORAGE = \
         'django.contrib.staticfiles.storage.StaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'INFO', # WARNING by default. Change this to capture more than warnings.
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'INFO', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
+
+"""
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -202,6 +249,30 @@ LOGGING = {
             'level': 'INFO',
         }
     }
-}
+}"""
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
+
+
+
+sentry_sdk.init(
+    dsn="https://d42e418337c0497ca27ee7012c862b71@o1055791.ingest.sentry.io/6041945",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+RAVEN_CONFIG = {
+    'dsn': 'https://somethingverylong@sentry.io/6041945', # caution replace by your own!!
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+}
+
---
+++ PurBeurre/settings_travis.py
@@ -0,0 +1,14 @@
+from PurBeurre.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+
+SECRET_KEY = "django-insecure-t*!rwp+hhb&q*sfb#+5#bsrgn9c5x"
