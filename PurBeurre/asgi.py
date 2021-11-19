9996b497393d002986012adb3070b8fa58e03595
RiwsPy <thibault.debruyne@hotmail.fr>
2021-11-19 11:09:51 +0100

    SETTINGS_FILE > SETTINGSFILE

--- PurBeurre/asgi.py
+++ PurBeurre/asgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('SETTINGS_FILE'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('SETTINGSFILE'))
 
 application = get_asgi_application()
--- PurBeurre/wsgi.py
+++ PurBeurre/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('SETTINGS_FILE'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('SETTINGSFILE'))
 
 application = get_wsgi_application()
